d161b2049fcc5ff14a082b7a40dcdcd9fc00b48c
Maximilian Eham <maximilian-eham@gmx.net>
2018-04-21 16:32:20 +0200

    create inital react components
    components: Aggregate, Filter, Playlist

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let defaultStyle = {
+  color: '#fff',
+}
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return(
+      <div style={defaultStyle}>
+        <img/>
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return(
+      <div style={{...defaultStyle, display: "inline-block", width: "25%"}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    let name = 'Max'
-    let color = 'lightblue'
-    let headerStyle = { color: color, 'font-size': '50px'}
     return (
       <div className="App">
-        <h1>Title</h1>
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 style={headerStyle} className="App-title">Welcome {name}! JS will be the last programming language you will use, whether you want to or not >:)</h1>}
-        </header>
+        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
       </div>
     );
   }
